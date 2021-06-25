d139b005d389185385c13428a12d8d77a93bbe4f
AlexMontgomerie <am9215@ic.ac.uk>
2021-06-25 15:19:46 +0000

    convolution layer passing tests

--- include/accum.hpp
+++ include/accum.hpp
@@ -1,5 +1,5 @@
-#ifndef ACCUM_HPP_ 
-#define ACCUM_HPP_ 
+#ifndef ACCUM_HPP_
+#define ACCUM_HPP_
 
 #include "common.hpp"
 
@@ -17,40 +17,37 @@ void accum(
 )
 {
 
-    #pragma HLS INLINE OFF 
-    
+    #pragma HLS INLINE OFF
+
     const unsigned int batch_size = BATCH_SIZE;
     const unsigned int rows       = ROWS;
     const unsigned int cols       = COLS;
     const unsigned int channels   = CHANNELS;
     const unsigned int filters    = FILTERS;
     const unsigned int groups     = GROUPS;
-    
+
     const unsigned int channels_per_group = DIVIDE(channels,groups);
     const unsigned int filters_per_group  = DIVIDE(filters ,groups);
 
     #pragma HLS STREAM variable=in
     #pragma HLS STREAM variable=out
     DO_PRAGMA(HLS STREAM variable=out depth=filters_per_group+1)
-    
+
     acc_t acc[filters_per_group];
-    #pragma HLS dependence variable=acc intra RAW true
+    #pragma HLS dependence variable=acc intra WAR true
     #pragma HLS resource variable=acc core=RAM_2P_BRAM
 
-    stream_pixel_loop: for(unsigned long pixel_index=0;pixel_index<batch_size*rows*cols;pixel_index++) {
-        stream_group_loop: for(unsigned int group_index=0;group_index<groups;group_index++) {
-            stream_channel_loop: for(unsigned int channel_index=0;channel_index<channels_per_group;channel_index++) {
-                while( out.full() ) {}; 
-                stream_filter_loop: for(unsigned int filter_index=0;filter_index<filters_per_group;filter_index++) {
-                    #pragma HLS PIPELINE II=1 rewind 
-                   
-                    acc_t prev = ( channel_index == 0 ) ? acc_t(0) : acc[filter_index] ;
-                    acc[filter_index] = prev + in.read() ;
-                
-                    if( channel_index == (channels_per_group-1) ) {
-                        out.write( acc[filter_index] ) ;
-                    }
+    stream_pixel_loop: for(unsigned long pixel_index=0;pixel_index<batch_size*rows*cols*groups;pixel_index++) {
+        stream_channel_loop: for(unsigned int channel_index=0;channel_index<channels_per_group;channel_index++) {
+            /* while( out.full() ) {}; */
+            stream_filter_loop: for(unsigned int filter_index=0;filter_index<filters_per_group;filter_index++) {
+                #pragma HLS PIPELINE II=1 rewind
+                acc_t prev = ( channel_index == 0 ) ? acc_t(0) : acc[filter_index] ;
+                acc_t curr =  prev + in.read();
+                acc[filter_index] = curr;
 
+                if( channel_index == (channels_per_group-1) ) {
+                    out.write( curr ) ;
                 }
             }
         }
